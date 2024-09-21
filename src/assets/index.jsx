641df2f5d72632dd3011c569b317d599ca6ae400
dilpr <dilpreetbajwa1@gmail.com>
2024-09-21 18:05:14 -0400

    portfolio

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,5 +2,6 @@ import logo from "./images/logo.png";
 import profileImg from "./images/profile.png";
 import githubcover from "./images/githubcover.png";
 import aiimage from "./images/aiimage.jpg";
+import portfolio from "./images/portfolio.png";
 
-export { logo, profileImg, githubcover, aiimage };
+export { logo, profileImg, githubcover, aiimage, portfolio };
--- src/ui/Project.jsx
+++ src/ui/Project.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from "./Container";
 import SectionTitle from "./SectionTitle";
-import { githubcover, aiimage } from "../assets";
+import { githubcover, aiimage, portfolio } from "../assets";
 import ProjectsLInks from "./ProjectsLInks";
 
 const Project = () => {
@@ -83,7 +83,40 @@ const Project = () => {
           </div>
         </div>
         {/* Project Three */}
-       
+        <div className="flex flex-col lg:flex-row gap-6">
+          {/* Image */}
+          <a
+            href="https://github.com/dilpreetbajwa/coursera_meta-capstone-project"
+            target="blank"
+            className="w-full lg:w-1/2 h-auto relative group rounded-lg overflow-hidden"
+          >
+            <img
+              src={portfolio}
+              alt="githubcover"
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute w-full h-full bg-designColor/10 rounded-lg top-0 left-0 group-hover:bg-transparent duration-300" />
+          </a>
+          {/* Description */}
+          <div className="w-full lg:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right lg:-ml-16 z-10">
+            <div>
+              <h3 className="text-2xl font-bold">Portfolio</h3>
+            </div>
+            <p className="bg-textBg text-sm md:text-base p-2 md:p-6 rounded-md">
+               Created a personal portfolio using react.js including work experience and personal projects for github.{" "}
+              {/* <span className="text-textGreen">O-auth</span> and then make the
+              purchage using <span className="text-designColor">stripe</span>. */}
+            </p>
+            <ul className="text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-darkText">
+              <li>React.js</li>
+              <li>Javascript</li>
+              <li>Tailwind</li>
+              <li>Vite</li>
+              
+            </ul>
+            <ProjectsLInks link="https://github.com/dilpreetbajwa/coursera_meta-capstone-project" />
+          </div>
+        </div>
       </div>
     </Container>
   );
